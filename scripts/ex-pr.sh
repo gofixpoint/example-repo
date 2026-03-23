c23c211c99b6e161ed8152a6ae5211b475bdee92
Dylan Mikus <dylan@fixpoint.co>
2026-03-23 15:28:20 -0400

    Adjust example script and make better python version
    Adjust the bash example script to try to get it to work better. There
    are weird quoting issues and stuff like that, so I created another
    version that was Python. That version works.

--- scripts/ex-pr.sh
+++ scripts/ex-pr.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-set -euo pipefail
+# set -euo pipefail
 
 errcho() { echo "$@" >&2; }
 
 errcho "Creating sandbox..."
-amika-local sandbox create --git --secret env:ANTHROPIC_API_KEY --name testing-claude-api
+amika sandbox create --git --secret env:ANTHROPIC_API_KEY --name testing-claude-api
 
-CLAUDE_SETTINGS='
+export CLAUDE_SETTINGS='
 {
     "permissions": {
         "allow": [
@@ -17,43 +17,38 @@ CLAUDE_SETTINGS='
     }
 }'
 
-CLAUDE_GITIGNORE="settings.local.json"
+export CLAUDE_GITIGNORE="settings.local.json"
 
 errcho "Configuring Claude settings in sandbox..."
-amika-local sandbox ssh testing-claude-api << EOF 2>/dev/null
+amika sandbox ssh testing-claude-api << EOF 2>/dev/null
 mkdir -p \$AMIKA_AGENT_CWD/.claude
 echo '$CLAUDE_GITIGNORE' > \$AMIKA_AGENT_CWD/.claude/.gitignore
 echo '$CLAUDE_SETTINGS' > \$AMIKA_AGENT_CWD/.claude/settings.local.json
 exit 0
-EOF
+EOF || true
 
 
 
-CLAUDE_UUID=$(uuidgen)
+export CLAUDE_UUID=$(uuidgen)
 
 errcho "Generated Claude session ID: $CLAUDE_UUID"
 
-CLAUDE_PROMPT="Change the heading text to also include, 'Dylan was here!' You should make this change on a new git branch, and when you are done you should commit the branch and make a Github PR. Do this in one shot. Do not ask me any questions. Make sure to also git commit your changes and then make a PR. To reiterate, do these in order:
-
-1. create a new git branch
-2. make the code changes
-3. commit your code changes
-4. make a Github PR with the changes
+export CLAUDE_PROMPT="Change the heading text to also include, 'Dylan was here!'
 
 Do not stop, do not ask me for clarification or questions.
 "
 
 errcho "Running Claude to make code changes..."
-amika-local sandbox ssh -t testing-claude-api << EOF 2>/dev/null
+amika sandbox ssh -t testing-claude-api << EOF 2>/dev/null
 cd \$AMIKA_AGENT_CWD && claude --dangerously-skip-permissions --session-id '$CLAUDE_UUID' -p '$CLAUDE_PROMPT' < /dev/null
 exit 0
-EOF
+EOF || true
 
 # For some reason, Claude does not like single-shotting both the code changes and the git branch/commit/PR. So we need to do it in two steps.
 errcho "Resuming Claude to create git branch and PR..."
-amika-local sandbox ssh -t testing-claude-api << EOF 2>/dev/null
+amika sandbox ssh -t testing-claude-api << EOF 2>/dev/null
 cd \$AMIKA_AGENT_CWD && claude --dangerously-skip-permissions --resume '$CLAUDE_UUID' -p 'Make a git branch and commit the changes. Then make a Github PR' < /dev/null
 exit 0
-EOF
+EOF || true
 
 errcho "Done."
